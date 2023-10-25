b6464dd861bef5e2ee3a228393d4d22207a58b35
IVskiller <112958685+IVskiller@users.noreply.github.com>
2023-10-25 11:35:35 +0200

    Add files via upload

--- knjiznica/knjiznica/Form1.cs
+++ knjiznica/knjiznica/Form1.cs
@@ -16,16 +16,25 @@ namespace knjiznica
     {
         public class knjiznica
         {
-            List<Clan> clanovi = new List<Clan>();
-            List<Knjiga> knjige = new List<Knjiga>();
+            public List<Clan> clanovi = new List<Clan>();
+            public List<Knjiga> knjige = new List<Knjiga>();
         }
         public class Knjiga
         {
             protected int ID;
-            static int IDbrojac;
+            static int IDbrojac=0;
             protected string ime;
             protected string autorime;
             protected string autorprezime;
+
+            public Knjiga(string ime_,string autorime_, string autorprezime_) {
+                ID = IDbrojac;
+                ime = ime_;
+                autorime = autorime_;
+                autorprezime = autorprezime_;
+                IDbrojac++;
+            }
+
         }
 
         public class Clan
@@ -34,10 +43,21 @@ namespace knjiznica
             static int IDbrojac;
             protected string ime;
             protected string prezime;
+            public Clan(string ime_, string prezime_)
+            {
+                OIB = IDbrojac;
+                ime = ime_;
+                prezime = prezime_;
+                IDbrojac++;
+            }
+
         }
         public Form1()
         {
             InitializeComponent();
+            Knjiga knj1 = new Knjiga("hlapić","ivana","brlić");
+
+
         }
 
     }
